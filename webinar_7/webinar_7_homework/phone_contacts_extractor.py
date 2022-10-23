30915bb7697d6568e74fb18ba69c7169a3d821d4
Pablo <i@kotelnikovpa.ru>
2022-10-23 21:46:41 +0300

    corrected export function

--- webinar_7/webinar_7_homework/phone_contacts_extractor.py
+++ webinar_7/webinar_7_homework/phone_contacts_extractor.py
@@ -3,7 +3,7 @@ FILE_NAME = "/home/pablo/Documents/Python GB/Block_2_Python/webinar_7/webinar_7_
 
 def write_contact(new_data):
     with open(FILE_NAME, 'a', encoding= "UTF-8") as f:
-         f.writelines(f"\n{new_data[0]}, {new_data[1]}, {new_data[2]}, {new_data[3]}\n")
+         f.writelines(f"\n{new_data[0]}, {new_data[1]}, {new_data[2]}, {new_data[3]}")
 
 
 def read_phone_book():
